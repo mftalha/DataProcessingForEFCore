ac06be001f994fc0daea625a23e8fea5b3863c13
Talha Satır <imnever0729@gmail.com>
2023-06-13 02:43:14 +0300

    Feat - Temel düzey veri sorgulama.  IQuaryable , IEnumarable,  diferred execution

---
+++ Quering/Migrations/20230612224100_mig_1.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quering.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PartName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Parts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Parts_ProductId",
+                table: "Parts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parts");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
--- Quering/Program.cs
+++ Quering/Program.cs
@@ -1,24 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.EntityFrameworkCore;
 
-Console.WriteLine("Hello, World!");
+
+QueringClass context = new();
 
 #region En temel basit bir sorgulama nasıl yapılır?
+#region Method Syntex 
+// Link fonksiyonları
+//var products = await context.Products.ToListAsync();
+#endregion
+#region Query Syntax
+// Link Query
+
+//var products2 = await (from product in context.Products select product).ToListAsync();
+#endregion
+#endregion
+#region Sorguları Execute etmek için ne yapmamız gerekmektedir ?
+
+#region ToListAsync
+#endregion
+
+int productId = 5;
+var products = from product in context.Products
+               where product.Id > productId
+               select product;
+
+productId = 200;
+//geciktirilmiş sorgu(yürütme) : buraya kadar sorguyu göndermiyordu. : çünkü daha çağrılmamıştı : diferred execution(ertelenmiş çalışma)
+#region Foreach
+foreach (Product product in products) 
+{
+    Console.WriteLine(product.ProductName);
+}
+
+#region Deferred Execution(Ertelenmiş çalışma)
+/*
+   - IQueryable çalışmalarında ilgili kod yazıldığı noktada tetiklenemez/çalıştırılmaz yani ilgili kod yazıldığı noktada soruguyu generate etmez! Nerede eder? Çalıştırıldığı/execute edildiği noktada tetiklenir! işte bu durumda ertelenmiş çalışma denir!
+   - ertelenmiş çalışmanın mantığı : veriler Linq ile sorgulanduğında değilde : ilgili veriler kullanılacağı zaman çekilir. => kullanma mantığıda data'yı Enumarable'ye çevirme anı veya for each içinde kullanma anıdır.
+*/
+#endregion
+#endregion
+#endregion
+#region IQueryable ve IEnumerable Nedir? Basit olarak!
+
+//var products = await (from product in context.Products select product).ToListAsync(); // ToListAsync => ıquaryable sorgusundaki verileri almak için
+#region IQueryable
+/*
+    Sorguya karşılık gelir
+    EF Core üzerinden yapılmış olan sorgunun execute edilmemiş halini ifade eder
+*/
+#endregion
+#region IEnumerable
+/*
+    Sorgunun çalıştırılıp/execute edilip verilerin in memoruye yüklenmiş halini efade eder.
+*/
+#endregion
+#endregion
+#region Çoğul veri getiren sorgulama fonksiyonları
+#endregion
+#region Tekil veri getiren sorgulama fonksiyonları
+#endregion
+#region Diger sorgulama fonksiyonları
+#endregion
+#region Sorgu sonucu dönüşüm fonksiyonları
 
 #endregion
 
 public class QueringClass : DbContext
 {
-    DbSet<Student> Students { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Part> Parts { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=TestDb;Trusted_Connection=True;TrustServerCertificate=True");
     }
 }
 
-public class Student
+public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string City { get; set; }
+    public string ProductName { get; set; }   
+    public float Price { get; set; }
+    public ICollection<Part> Parts { get; set; }
+    
+}
+public class Part
+{
+    public int Id { get; set; }
+    public string PartName { get; set; }
 }
