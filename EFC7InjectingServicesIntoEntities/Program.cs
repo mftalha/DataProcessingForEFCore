78fefee3bb5d1bb24719b165a286a32be4165cf0
Talha <talha.satir@vinya.com.tr>
2023-08-04 03:32:00 +0300

    feat- Entity içierisinde servis kullanma gibi bir mantık.

---
+++ EFC7InjectingServicesIntoEntities/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+InjectingServicesIntoEntityDbContext context = new();
+// Entity içinde servis kullanma
+
+#region Entity'de Service Inject Etme
+var persons = await context.Persons.ToListAsync();
+foreach (var person in persons)
+{
+person.ToString();
+}
+#endregion
+
+public class PersonServiceInjectionInterceptor : IMaterializationInterceptor
+{
+    public object InitializedInstance(MaterializationInterceptionData materializationData, object instance)
+    {
+        if (instance is IHasPersonService hasPersonService)
+        {
+            hasPersonService.PersonService = new PersonLogService();
+        }
+
+        return instance;
+    }
+}
+public interface IHasPersonService
+{
+    IPersonLogService PersonService { get; set; }
+}
+public interface IPersonLogService
+{
+    void LogPerson(string name);
+}
+public class PersonLogService : IPersonLogService
+{
+    public void LogPerson(string name)
+    {
+        Console.WriteLine($"{name} isimli kişi loglanmıştır.");
+    }
+}
+public class Person : IHasPersonService
+{
+    public int PersonId { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString()
+    {
+        PersonService?.LogPerson(Name);
+        return base.ToString();
+    }
+
+    [NotMapped]
+    public IPersonLogService? PersonService { get; set; }
+}
+class InjectingServicesIntoEntityDbContext : DbContext
+{
+    public DbSet<Person> Persons { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder
+            .UseSqlServer("Server=localhost\\SQLEXPRESS;Database=InjectingServicesIntoEntityDb;User ID=SA;Password=123!;TrustServerCertificate=True");
+
+        optionsBuilder.AddInterceptors(new PersonServiceInjectionInterceptor());
+    }
+}
